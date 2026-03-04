15c8cc447f7d7d67abae739a6ed78cc0e67a04ca
filip-rs <filip.spanne@gmail.com>
2026-03-04 16:37:45 +0100

    Fully updated treesitter conf

--- dotfiles/dot-config/nvim/lua/filip/plugins/treesitter.lua
+++ dotfiles/dot-config/nvim/lua/filip/plugins/treesitter.lua
@@ -1,54 +1,79 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
-  build = ":TSUpdate",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    event = "BufReadPost",
+    build = ":TSUpdate",
+    config = function()
+      local ts = require("nvim-treesitter")
+
+      ts.setup({
+        ensure_installed = {
+          "lua", "vim", "vimdoc", "python", "go", "gomod",
+          "javascript", "typescript", "tsx", "json", "yaml",
+          "html", "css", "bash", "markdown", "markdown_inline",
+        },
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+
+      -- Install missing parsers
+      local installed = ts.get_installed and ts.get_installed() or {}
+      local to_install = {}
+      for _, lang in ipairs({ "lua", "vim", "vimdoc", "python", "go", "javascript", "typescript", "json", "yaml", "html", "css", "bash", "markdown" }) do
+        if not vim.tbl_contains(installed, lang) then
+          table.insert(to_install, lang)
+        end
+      end
+      if #to_install > 0 and ts.install then
+        ts.install(to_install)
+      end
+
+      -- Enable treesitter highlighting for buffers
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          pcall(vim.treesitter.start, ev.buf)
+        end,
+      })
+    end,
   },
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require("nvim-treesitter").setup({
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "python",
-        "go",
-        "gomod",
-        "javascript",
-        "typescript",
-        "tsx",
-        "json",
-        "yaml",
-        "html",
-        "css",
-        "bash",
-        "markdown",
-        "markdown_inline",
-      },
-    })
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    event = "BufReadPost",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    config = function()
+      local ts_textobjects = require("nvim-treesitter-textobjects")
 
-    -- Enable treesitter features
-    vim.treesitter.language.register("bash", "sh")
-
-    -- Incremental selection keymaps
-    vim.keymap.set("n", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").init_selection()
-    end, { desc = "Init selection" })
-    vim.keymap.set("x", "<C-space>", function()
-      require("nvim-treesitter.incremental_selection").node_incremental()
-    end, { desc = "Increment selection" })
-    vim.keymap.set("x", "<bs>", function()
-      require("nvim-treesitter.incremental_selection").node_decremental()
-    end, { desc = "Decrement selection" })
-
-    -- Textobjects
-    local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move_next)
-    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_previous)
-
-    -- Setup autotag
-    require("nvim-ts-autotag").setup()
-  end,
+      if ts_textobjects.setup then
+        ts_textobjects.setup({
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true,
+            keys = {
+              goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+              goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+            },
+          },
+        })
+      end
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "BufReadPost",
+    opts = {},
+  },
 }
